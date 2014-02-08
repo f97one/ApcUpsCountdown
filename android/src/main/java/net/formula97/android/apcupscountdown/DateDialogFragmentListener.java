6f7820935d3985db93f0013bb0ba3ce8227d24b6
HAJIME Fukuna <f97one@gmail.com>
2014-02-08 20:10:00 +0900

    Interfaceに実装すべき内容を追加。

--- android/src/main/java/net/formula97/android/apcupscountdown/DateDialogFragmentListener.java
+++ android/src/main/java/net/formula97/android/apcupscountdown/DateDialogFragmentListener.java
@@ -1,7 +1,10 @@
 package net.formula97.android.apcupscountdown;
 
+import java.util.Calendar;
+
 /**
  * Created by HAJIME on 14/02/08.
  */
 public interface DateDialogFragmentListener {
+    public void dateDialogFragmentDateSet(Calendar date);
 }

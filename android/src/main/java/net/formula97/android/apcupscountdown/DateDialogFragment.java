e52b54882aa433cedc05b886b759744c3a2fea9d
HAJIME Fukuna <f97one@gmail.com>
2014-02-09 19:57:52 +0900

    Fragmentを使わずActivityだけに集約することにしたので、実装を中止。

--- android/src/main/java/net/formula97/android/apcupscountdown/DateDialogFragment.java
+++
@@ -1,9 +0,0 @@
-package net.formula97.android.apcupscountdown;
-
-import android.app.DialogFragment;
-
-/**
- * Created by HAJIME on 14/02/08.
- */
-public class DateDialogFragment extends DialogFragment {
-}
